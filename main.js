562595882bd88eb1914513db00752ad528a938de
AaaAkita <761749068@qq.com>
2026-03-17 19:56:21 +0800

    第二版：添加照片自动检测功能，支持任意图片名称，无图片时显示占位符

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
 new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        photoUrl: ''
+    },
+    mounted() {
+        this.loadPhoto();
+    },
+    methods: {
+        loadPhoto() {
+            const possibleNames = ['photo.jpg', 'photo.png', 'photo.jpeg', 'photo.webp',
+                                   'avatar.jpg', 'avatar.png', 'avatar.jpeg', 'avatar.webp',
+                                   'profile.jpg', 'profile.png', 'profile.jpeg', 'profile.webp',
+                                   'head.jpg', 'head.png', 'head.jpeg', 'head.webp',
+                                   '1.jpg', '1.png', '1.jpeg', '1.webp',
+                                   '2.jpg', '2.png', '2.jpeg', '2.webp'];
+
+            const tryLoadImage = (index) => {
+                if (index >= possibleNames.length) {
+                    this.photoUrl = '';
+                    return;
+                }
+
+                const img = new Image();
+                img.onload = () => {
+                    this.photoUrl = 'images/' + possibleNames[index];
+                };
+                img.onerror = () => {
+                    tryLoadImage(index + 1);
+                };
+                img.src = 'images/' + possibleNames[index];
+            };
+
+            tryLoadImage(0);
+        }
+    }
 })
